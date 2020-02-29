64dea696683921d73965ca99f8f6c5b8a649ee10
Valentin P <valentin.porcellini@gmail.com>
2020-02-29 17:21:25 -0500

    Added dashboard main files

---
+++ dashboard/datavis.js
@@ -0,0 +1,13 @@
+const svg = d3.select("svg1")
+    .append("svg")
+    .attr("width", 700)
+    .attr("height", 300);
+
+const width = svg.attr("width");
+const height = svg.attr("height");
+
+svg.append("rect")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("fill", "grey");
+
